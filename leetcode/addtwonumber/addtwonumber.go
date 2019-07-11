7705fe288d2cedec97b23d3d4c62c8c488f77cf5
young <xiuyoung89@163.com>
2019-07-11 14:57:00 +0800

    addtwonumber

---
+++ leetcode/addtwonumber/addtwonumber.go
@@ -0,0 +1,48 @@
+/*
+@Time : 19-7-11 下午1:52
+@Author : young
+@File : addtwonumber
+@Software: GoLand
+*/
+package addtwonumber
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func AddTwoNumber(l1 *ListNode, l2 *ListNode) *ListNode {
+	res := &ListNode{}
+
+	cur := res
+
+	c := 0
+
+	for l1 != nil || l2 != nil || c != 0 {
+
+		if l1 != nil {
+			cur.Val += l1.Val
+			l1 = l1.Next
+		}
+
+		if l2 != nil {
+			cur.Val += l2.Val
+			l2 = l2.Next
+		}
+
+		cur.Val += c
+
+		c = cur.Val/10
+		cur.Val %= 10
+
+		if l1 == nil && l2 == nil && c == 0 {
+			return  res
+		}
+
+		cur.Next = &ListNode{}
+		cur = cur.Next
+
+	}
+
+	return  nil
+}
