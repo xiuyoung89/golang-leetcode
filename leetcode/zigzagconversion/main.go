8365db0e1cf28097bb4851e8b2cf3729310b4a9d
young <xiuyoung89@163.com>
2020-06-16 13:56:20 +0800

    add reverse integer

---
+++ leetcode/zigzagconversion/main.go
@@ -0,0 +1,43 @@
+/*
+   @Time : 2020/6/16 上午10:48
+   @Author : young
+   @File : main.go
+   @Software: GoLand
+*/
+package main
+
+import "log"
+
+func main() {
+	log.Printf("this is zigzag conversion.")
+
+	var s = "AB"
+	ret := convert(s, 1)
+	log.Printf("get ret: %s", ret)
+}
+
+func convert(s string, numRows int) string {
+
+	if len(s) < numRows || numRows ==1{
+		return s
+	}
+
+	var tmp = make([]string, numRows)
+	var ret string
+	revSign := 1
+	rowStep := 0
+	for _, v := range s {
+
+		tmp[rowStep] += string(v)
+		rowStep += revSign
+		if rowStep == 0 || rowStep == numRows-1 {
+			revSign = -revSign
+		}
+	}
+
+	//合并数据
+	for j, _ := range tmp {
+		ret += tmp[j]
+	}
+	return ret
+}
