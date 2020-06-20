0eb8dd50756de9abf45cfd2c4dee5c3d6f3bfde3
Young <xiuyoung89@163.com>
2020-06-20 14:31:24 +0800

    update something

---
+++ leetcode/42raintrap/main.go
@@ -0,0 +1,17 @@
+/*
+   @Time : 20-6-16 下午11:12
+   @Author : young
+   @File : main.go
+   @Software: GoLand
+*/
+package main
+
+import "log"
+
+func main() {
+	log.Println("42 rain trap ")
+}
+
+func trap(height []int) int {
+
+}
